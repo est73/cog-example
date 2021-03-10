a77764dc0901868853ea3b0769725c23c78a22b4
est73 <brianross@outlook.com>
2021-03-10 03:57:38 -0800

    Update routes.py

--- cogs/routes.py
+++ cogs/routes.py
@@ -18,8 +18,7 @@ class Routes(commands.Cog):
 
     @tasks.loop()
     async def api(self):
-        options = {'bind': '127.0.0.1:5000',
-                   'insecure-bind': '127.0.0.1:8000',
+        options = {'bind': '127.0.0.1:8000',
                    'certfile': None, # path to cert
                    'keyfile': None, # path to key
                    'accesslog': 'access.log',
